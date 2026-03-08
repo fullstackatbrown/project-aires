6e44b9d6f2af0e0603cbdefe6fab4ee07b7e15df
dvass26 <david_vassalluzzo@brown.edu>
2026-03-08 16:35:36 -0400

    initial

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,20 @@
+import React from "react";
+import logo from "../../public/aires-logo.png";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import "./about.css";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 export default function HomePage() {
-  return (
-    <main>
-      <h1>About</h1>
+    return (
+    <main className={`background-color-background ${montserrat.variable} font-sans`}>
+      <div className="flex flex-col items-center justify-center min-h-screen py-0.5 bg-white">
+        <Image src={logo} alt="AIRES Logo" width={100} height={100} className="image-spacing" />
+        <h1 className="text-4xl text-spacing font-bold text-[#08B2E3]">About AIRES</h1>
+      </div>
     </main>
   );
 }
