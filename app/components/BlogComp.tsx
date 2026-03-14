9d34d1d13f0f2ee1d88ca1e60db240dadcef7e4a
demed223 <daniel_demessie@brown.edu>
2026-03-14 04:22:56 -0400

    did a lot of the home page

---
+++ app/components/BlogComp.tsx
@@ -0,0 +1,35 @@
+import ReadMore from "./ReadMore";
+
+type BlogCompProps = {
+  title: string;
+  author: string;
+  date: string;
+  imageSrc?: string;
+  imageAlt?: string;
+};
+
+export default function BlogComp({
+  title,
+  author,
+  date,
+  imageSrc = "/PBlog.png",
+  imageAlt = "Blog thumbnail",
+}: BlogCompProps) {
+  return (
+    <div className="flex items-center gap-4 p-4 mb-4 w-[572px] min-h-[163px]">
+      <img
+        src={imageSrc}
+        alt={imageAlt}
+        className="w-[163px] h-[163px] object-cover rounded-2xl shrink-0"
+      />
+
+      <div className="flex flex-col justify-center h-[163px] gap-1">
+        <p className="text-sm text-gray-600">
+          {author} | {date}
+        </p>
+        <h2 className="text-xl font-semibold text-black leading-tight">{title}</h2>
+        <ReadMore href="/blog" className="mt-1" />
+      </div>
+    </div>
+  );
+}
