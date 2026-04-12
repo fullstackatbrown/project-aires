01a0048444555deaffd257b287449f76e738898d
zzmic <zzmicDev@gmail.com>
2026-04-12 09:28:58 -0400

    1. Define and register `eBoardType`
    2. Refine `authorType`

--- sanity-cms/schemaTypes/index.ts
+++ sanity-cms/schemaTypes/index.ts
@@ -1,10 +1,11 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
 
-import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
+import { authorType } from "./authorType";
+import { blockContentType } from "./blockContentType";
+import { categoryType } from "./categoryType";
+import { eBoardType } from "./eBoardType";
+import { postType } from "./postType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
-}
+  types: [authorType, blockContentType, categoryType, eBoardType, postType],
+};
