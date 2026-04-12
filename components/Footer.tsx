03eff1cbde9b5a26d2be650fd9e9a4886c99f283
Roger Mao <rogermao2019@gmail.com>
2026-04-12 13:47:47 -0400

    updated footer with socials, waiting on pull to make links to other subpages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 "use client"; 
 import Image from "next/image"; 
-import Social from "@/components/LinkColumn"; 
 
 
 export default function Footer() {
@@ -26,7 +25,7 @@ export default function Footer() {
 
     return(
         <div className="flex flex-col bg-[#08B2E3]">
-            <div className="flex flex-row w-screen px-18 pt-10 pb-5">
+            <div className="flex flex-row justify-between w-screen px-18 pt-10 pb-5">
 
                 <div className="flex flex-row w-1/2 px-4">
                     <Image 
@@ -38,10 +37,21 @@ export default function Footer() {
                     <h1 className="pl-4 flex items-center h-full text-2xl text font-semibold">Creating AI Ethically and Responsibly.</h1>
                 </div>
                 
-                {/* will fillwith LinkColumn here */}
+                <div className="flex flex-row">
+                    <div className = "flex flex-col">
+                        <h1 className="font-semibold text-2xl mx-4 ">Key Links</h1>
 
+                    </div>
+
+                    <div className = "flex flex-col ml-4">
+                        <h1 className="font-semibold text-2xl mr-18">Social Media</h1>
+                        <a className = "transition-all text-lg my-1 font-semibold hover:text-amber-300" href="https://www.instagram.com/brownu_aires/" target="_blank">Instagram</a>
+                        <a className = "transition-all text-lg my-1 font-semibold hover:text-amber-300" href="mailto:aires@brown.edu?subject=Let%27s%20Get%20In%20Touch%21">Email</a>
+                        <a className = "transition-all text-lg my-1 font-semibold hover:text-amber-300" href="https://airesatbrown.substack.com/p/airesbrown-january-2026-newsletter">Substack</a>
+                    </div>
+                </div>
             </div>
-            <p className="font-light px-22">
+            <p className="font-light px-34 mb-5 text-right">
                 AIRES @ Brown. All Rights Reserved.
             </p>
         </div>
